289497646ac1a9c2ebebc8cbbef08259080009c8
Kvark <kvark128@yandex.ru>
2020-10-05 20:09:21 +0800

    First commit

---
+++ sonic.go
@@ -0,0 +1,71 @@
+package sonic
+
+//#include "sonic.h"
+import "C"
+
+import (
+	"errors"
+	"unsafe"
+)
+
+type Stream struct {
+	sampleRate int
+	channels   int
+	sampleSize int
+	speed      float64
+	stream     C.sonicStream
+}
+
+func NewStream(sampleRate, channels int) *Stream {
+	return &Stream{
+		sampleRate: sampleRate,
+		channels:   channels,
+		sampleSize: channels * 2,
+		speed:      1.0,
+		stream:     C.sonicCreateStream(C.int(sampleRate), C.int(channels)),
+	}
+}
+
+func (s *Stream) Write(data []byte) (int, error) {
+	nSamples := len(data) / s.sampleSize
+	if nSamples == 0 {
+		return 0, nil
+	}
+	ok := C.sonicWriteShortToStream(s.stream, (*C.short)(unsafe.Pointer(&data[0])), C.int(nSamples))
+	if ok == 0 {
+		return 0, errors.New("memory realloc failed")
+	}
+	return nSamples * s.sampleSize, nil
+}
+
+func (s *Stream) Read(data []byte) (int, error) {
+	nSamples := len(data) / s.sampleSize
+	if nSamples == 0 {
+		return 0, nil
+	}
+	readSamples := C.sonicReadShortFromStream(s.stream, (*C.short)(unsafe.Pointer(&data[0])), C.int(nSamples))
+	return int(readSamples) * s.sampleSize, nil
+}
+
+func (s *Stream) SamplesAvailable() int {
+	nSamples := C.sonicSamplesAvailable(s.stream)
+	return int(nSamples)
+}
+
+func (s *Stream) Speed() float64 {
+	return s.speed
+}
+
+func (s *Stream) SetSpeed(speed float64) {
+	s.speed = speed
+	C.sonicSetSpeed(s.stream, C.float(s.speed))
+}
+
+func (s *Stream) Flush() int {
+	return int(C.sonicFlushStream(s.stream))
+}
+
+func (s *Stream) Close() error {
+	C.sonicDestroyStream(s.stream)
+	return nil
+}
